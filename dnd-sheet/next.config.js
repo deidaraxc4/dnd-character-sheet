46849f9ae6505b4ef1ea5e5f0b88f0824aa32f3c
Nick Tang <tangnick@live.com>
2022-09-08 11:33:21 -0400

    use anchor in character index

--- dnd-sheet/next.config.js
+++ dnd-sheet/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  assetPrefix: "./",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     loader: 'akamai',
