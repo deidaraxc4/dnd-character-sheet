f1d6316bfffa6c941f53df219451cc7ca4636048
Nick Tang <tangnick@live.com>
2022-09-08 10:56:16 -0400

    add base path to nextconfig

--- dnd-sheet/next.config.js
+++ dnd-sheet/next.config.js
@@ -2,7 +2,8 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: false,
-  assetPrefix: isProd ? "./dnd-character-sheet/" : "",
+  assetPrefix: isProd ? "/dnd-character-sheet" : "",
+  basePath: isProd ? "/dnd-character-sheet" : "",
   images: {
     loader: 'akamai',
     path: '',
