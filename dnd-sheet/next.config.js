068fd829870b2bb45323074df4ded933cd5855d2
Nick Tang <tangnick@live.com>
2022-09-08 10:16:43 -0400

    update asset prefix

--- dnd-sheet/next.config.js
+++ dnd-sheet/next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: false,
-  assetPrefix: "./",
+  assetPrefix: isProd ? "./dnd-character-sheet/" : "",
   images: {
     loader: 'akamai',
     path: '',
