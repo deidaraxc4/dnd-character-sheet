51aba49279dd837d39bf544ddc2101a2cd490c14
Nick Tang <tangnick@live.com>
2022-09-07 21:19:50 -0400

    configure nextjs for github pages

--- dnd-sheet/next.config.js
+++ dnd-sheet/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: false,
+  assetPrefix: isProd ? "dnd-character-sheet/" : ""
 }
 
 module.exports = nextConfig
