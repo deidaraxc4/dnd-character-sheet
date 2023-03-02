0f522ffc9100bcdb0e9ee7d6e4a4a6e84f5b8f8d
Nick Tang <tangnick@live.com>
2023-03-02 16:16:02 -0500

    format spell list

--- dnd-sheet/components/SpellsCodex.js
+++ dnd-sheet/components/SpellsCodex.js
@@ -13,11 +13,26 @@ const SpellsCodex = () => {
         <div className="grid grid-cols-2">
             <div className="m-2">
                 <input onChange={handleChange} className="placeholder:italic placeholder:text-slate-400 block w-full bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm mb-4" placeholder="Search for spells..." type="search" name="search"/>
+                <div className="flex flex-row items-stretch">
+                    <a className="border rounded py-0.5 basis-1/2 text-center">Name</a>
+                    <a className="border rounded py-0.5 basis-1/4 text-center">Level</a>
+                    <a className="border rounded py-0.5 basis-1/4 text-center">Time</a>
+                    <a className="border rounded py-0.5 basis-1/2 text-center">School</a>
+                </div>
 
                 {spellData.filter((spell) => {
                     return spell.name.toLowerCase().includes(searchField.toLowerCase());
                 }).map(spell => {
-                    return (<p>{spell.name}: {spell.level}</p>);
+                    return (
+                        <div>
+                            <a className="flex border-b-2 border-solid pb-0.5 px-1">
+                                <span className="basis-1/2 font-bold">{spell.name}</span>
+                                <span className="basis-1/4 font-light">{spell.level}</span>
+                                <span className="basis-1/4 font-light">{spell.casting_time}</span>
+                                <span className="basis-1/2 font-light">{spell.school}</span>
+                            </a>
+                        </div>
+                    );
                 })}
             </div>
 
