d56da65f34f5324271c7ed85a3892a5af0ef0105
Nick Tang <tangnick@live.com>
2023-03-02 18:54:35 -0500

    style and fix spell codex

--- dnd-sheet/components/SpellCard.js
+++ dnd-sheet/components/SpellCard.js
@@ -1,53 +1,58 @@
 import React, { useEffect, useState } from 'react';
 
 const SpellCard = ({spell}) => {
-    // const spell = props.spell;
-    console.log(spell)
-    return(
-        <h1>{spell.description}</h1>
-        // <table class="table-auto border-t-orange-400 border-b-orange-400 border-y-4 border-solid bg-orange-50 w-full">
-        //     <tr>
-        //         <th>
-        //             <h1>{spell.name}</h1>
-        //         </th>
-        //     </tr>
-        //     <tr>
-        //         <td>
-        //             <span className="italic">{spell.level}</span>
-        //         </td>
-        //     </tr>
-        //     <tr>
-        //         <td>
-        //             <span className="font-bold">Casting Time:</span>{spell.casting_time}
-        //         </td>
-        //     </tr>
-        //     <tr>
-        //         <td>
-        //             <span className="font-bold">Range:</span>{spell.range}
-        //         </td>
-        //     </tr>
-        //     <tr>
-        //         <td>
-        //             <span className="font-bold">Components:</span>{spell.components?.raw}
-        //         </td>
-        //     </tr>
-        //     <tr>
-        //         <td>
-        //             <span className="font-bold">Duration:</span>{spell.duration}
-        //         </td>
-        //     </tr>
 
-        //     <tbody className="border-t-2 border-solid border-orange-700">
-        //         <tr>
-        //             <p>{spell.description}</p>
-        //         </tr>
-        //         <tr>
-        //             <td>
-        //                 <span className="font-bold">Classes:</span> {spell.classes}
-        //             </td>
-        //         </tr>
-        //     </tbody>
-        // </table>
+    const type = spell.level == "cantrip" ? spell.school + " " + spell.level : "level " + spell.level + " " + spell.school;
+
+    return(
+        <table className="table-auto border-t-orange-400 border-b-orange-400 border-y-4 border-solid bg-orange-50 w-full">
+            <thead>
+            <tr>
+                <th>
+                    <h1 className="text-red-800 text-left text-4xl font-light font-serif p-1">{spell.name}</h1>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <span className="italic">
+                        {type}
+                    </span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span className="font-bold">Casting Time:</span> {spell.casting_time}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span className="font-bold">Range:</span> {spell.range}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span className="font-bold">Components:</span> {spell.components?.raw}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span className="font-bold">Duration:</span> {spell.duration}
+                </td>
+            </tr>
+            </thead>
+            <tbody className="border-t-2 border-solid border-orange-700">
+                <tr>
+                    <td className="p-2">
+                        <p>{spell.description}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span className="font-bold">Classes:</span> {spell.classes.join(",")}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     );
 }
 
--- dnd-sheet/components/SpellsCodex.js
+++ dnd-sheet/components/SpellsCodex.js
@@ -5,10 +5,14 @@ import SpellCard from './SpellCard';
 const SpellsCodex = () => {
     const [searchField, setSearchField] = useState("");
     const [selectedSpell, setSelectedSpell] = useState(spellData[0]);
-    // console.log(selectedSpell)
+
     const handleChange = (e) => {
         setSearchField(e.target.value);
     };
+
+    const handleSpellSelect = spell => () => {
+        setSelectedSpell(spell)
+    }
 
     return(
         <div className="grid grid-cols-2">
@@ -24,9 +28,10 @@ const SpellsCodex = () => {
                 {spellData.filter((spell) => {
                     return spell.name.toLowerCase().includes(searchField.toLowerCase());
                 }).map(spell => {
+                    const tag = "#" + spell.name
                     return (
-                        <div>
-                            <a className="flex border-b-2 border-solid pb-0.5 px-1">
+                        <div key={spell.name}>
+                            <a href={tag} className="flex border-b-2 border-solid pb-0.5 px-1 cursor-pointer hover:bg-slate-200 focus:bg-sky-400" onClick={handleSpellSelect(spell)}>
                                 <span className="basis-1/2 font-bold">{spell.name}</span>
                                 <span className="basis-1/4 font-light">{spell.level}</span>
                                 <span className="basis-1/4 font-light">{spell.casting_time}</span>
